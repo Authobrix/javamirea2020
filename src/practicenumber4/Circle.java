19c65deabc0a5299c5c0c2aa1202d985636f8c6b
Danya <sologubdanil2001@gmail.com>
2020-09-20 16:30:42 +0300

    added practice 4

--- src/practicenumber4/Circle.java
+++ src/practicenumber4/Circle.java
@@ -27,12 +27,22 @@ package practicenumber4;
         }
 
         @Override
+        public double getArea()
+        {
+            return 3.14*radius*radius;
+        }
+        @Override
+        public double getPerimeter()
+        {
+            return 2*3.14*radius;
+        }
+
+        @Override
         public String toString() {
             return "Circle{" +
                     "radius=" + radius +
+                    ", color='" + color + '\'' +
+                    ", filled=" + filled +
                     '}';
         }
-        public abstract double getArea();
-        public abstract double getPerimeter();
     }
-
